e6a85fca7d73264cda4aaf463a1a39c5933736e2
OUSMANE MERIGA <ousmane.meriga12@gmail.com>
2024-01-26 07:10:15 +0100

    header and footer inclusions

---
+++ build/footer.php
@@ -0,0 +1,30 @@
+
+<footer class="pt-4">
+          <div class="w-full px-6 mx-auto">
+            <div class="flex flex-wrap items-center -mx-3 lg:justify-center ">
+              <div class="w-full max-w-full px-3 mt-0 mb-6 shrink-0 lg:mb-0 lg:w-full ">
+                <div class="text-sm leading-normal text-center text-slate-500 lg:text-center">
+                  ©
+                  <script>
+                    document.write(new Date().getFullYear() + ",");
+                  </script>
+                  par
+                  <a href="#" class="font-semibold text-slate-700 dark:text-white" target="_blank">MyTavelr.</a>
+                  
+                </div>
+              </div>
+             
+            </div>
+          </div>
+        </footer>
+      </div>
+      <!-- end cards -->
+    </main>
+    
+    </div>
+  </body>
+  <!-- plugin for scrollbar  -->
+  <script src="../build/assets/js/plugins/perfect-scrollbar.min.js" async></script>
+  <!-- main script file  -->
+  <script src="../build/assets/js/argon-dashboard-tailwind.js?v=1.0.1" async></script>
+</html>
